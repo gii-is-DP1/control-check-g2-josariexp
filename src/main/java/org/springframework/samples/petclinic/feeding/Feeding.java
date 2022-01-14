4afc0a8d5890279df1219bda9418a7d305f4938d
Jose Ramon <jrae1112jrae@gmail.com>
2022-01-14 12:23:41 +0100

    commit 1

--- src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
@@ -2,6 +2,15 @@ package org.springframework.samples.petclinic.feeding;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.Pet;
 
 import lombok.Getter;
@@ -9,9 +18,24 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class Feeding {
-    Integer id;
+@Entity
+public class Feeding extends BaseEntity {
+	
+	@NotNull
+	@Column(name = "start_date")        
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
     LocalDate startDate;
+    
+	@Column(name = "weeks_duration")        
+    @NotNull
+    @Min(1)
     double weeksDuration;
+	
+	@NotNull
+	@ManyToOne(optional = false)
     Pet pet;   
+	
+	@ManyToOne
+	@JoinColumn(name = "feeding_type")
+    FeedingType feedingType;
 }
--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingController.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingController.java
@@ -1,5 +1,80 @@
 package org.springframework.samples.petclinic.feeding;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.pet.PetService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/feeding")
 public class FeedingController {
-    
+	
+	private static final String VIEWS_FEEDING_CREATE_OR_UPDATE_FORM = "feedings/createOrUpdateFeedingForm";
+
+	private final FeedingService feedingService;
+	private final PetService petService;
+	
+	@Autowired
+	public FeedingController(FeedingService feedingService, PetService petService) {
+		this.feedingService = feedingService;
+		this.petService = petService;
+	}
+	
+	@GetMapping(value = "/create")
+	public String initCreationForm(ModelMap modelMap) {
+		String view = VIEWS_FEEDING_CREATE_OR_UPDATE_FORM;
+		modelMap.addAttribute("feeding", new Feeding());
+		modelMap.addAttribute("feedingTypes", feedingService.getAllFeedingTypes());
+		modelMap.addAttribute("pets", petService.getAllPets());
+		return view;
+	}
+	
+	@PostMapping(value="/create")
+	public String processCreationForm(@Valid Feeding feeding, BindingResult result, ModelMap modelMap ) {
+		String view = "welcome";
+		if(result.hasErrors()) {
+			modelMap.addAttribute("feeding", feeding);
+			modelMap.addAttribute("feedingTypes", feedingService.getAllFeedingTypes());
+			modelMap.addAttribute("pets", petService.getAllPets());
+			return VIEWS_FEEDING_CREATE_OR_UPDATE_FORM;
+		}
+		else {
+			try {
+				feedingService.save(feeding);
+			} catch (UnfeasibleFeedingException e) {
+				// TODO Auto-generated catch block
+				modelMap.addAttribute("message", "La mascota seleccionada no se le puede asignar el plan de alimentación especificado.");
+				modelMap.addAttribute("feeding", feeding);
+				modelMap.addAttribute("feedingTypes", feedingService.getAllFeedingTypes());
+				modelMap.addAttribute("pets", petService.getAllPets());
+				return VIEWS_FEEDING_CREATE_OR_UPDATE_FORM;
+			}
+			modelMap.addAttribute("message", "Feeding successfully saves!");
+		}
+		return view;
+	}
+	
+
+//	@PostMapping(value = "/create")
+//	public String processCreationForm(@Valid Product product, BindingResult result, ModelMap modelMap ) {
+//		String view = "welcome";
+//		if(result.hasErrors()) {
+//			modelMap.addAttribute("product", product);
+//			modelMap.addAttribute("productType", productService.findAllProductTypes());
+//			return VIEWS_PRODUCT_CREATE_OR_UPDATE_FORM;
+//		}
+//		else {
+//			productService.save(product);
+//			modelMap.addAttribute("message", "Product saved succesfully!");
+//		}
+//		return view;
+//	}
+	
+   
 }
